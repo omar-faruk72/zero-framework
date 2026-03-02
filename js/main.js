8fc2fd6d7eb7122a3ddcaec5b6f8d1c498dad9c9
omar-faruk72 <mdfaruk01720875872@gmail.com>
2026-03-02 11:13:12 +0600

    adden an button succeefully added

--- js/main.js
+++ js/main.js
@@ -1,23 +1,29 @@
-async function loadLayout() {
-    try{
-
-   // ১. হেডার লোড করা
-        const headerPlaceholder = document.getElementById('header-placeholder');
-        if (headerPlaceholder) {
-            const res = await fetch('/shared/header.html');
-            headerPlaceholder.innerHTML = await res.text();
+async function loadComponent(id, path) {
+    const element = document.getElementById(id);
+    if (element) {
+        try {
+            const res = await fetch(path);
+            if (res.ok) {
+                element.innerHTML = await res.text();
+            } else {
+                console.error(`${path} `);
+            }
+        } catch (err) {
+            console.error(`${id} `, err);
         }
-
-        // footer load
-        const footerPlaceholder = document.getElementById('footer-placeholder');
-        if(footerPlaceholder) {
-            footerPlaceholder.innerHTML = await Response.text();
-        };
-
-    }catch(err) {
-        console.log("layout load error", err)
     }
+}
 
-};
+async function loadLayout() {
+    // header lod
+    await loadComponent('header-placeholder', '/shared/header.html');
+
+    // footer load
+    await loadComponent('footer-placeholder', '/shared/footer.html');
+
+    // button load
+    await loadComponent('hero-btn-solid', '/shared/btn-solid.html');
+    await loadComponent('hero-btn-outline', '/shared/btn-outline.html');
+}
 
 loadLayout();
