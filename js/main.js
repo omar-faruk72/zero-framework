c14d59f44b44c2721a25cf7db8a7742415127139
omar-faruk72 <mdfaruk01720875872@gmail.com>
2026-03-02 10:25:56 +0600

    added an header code and css connection complite

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 async function loadLayout() {
     try{
 
+   // ১. হেডার লোড করা
+        const headerPlaceholder = document.getElementById('header-placeholder');
+        if (headerPlaceholder) {
+            const res = await fetch('/shared/header.html');
+            headerPlaceholder.innerHTML = await res.text();
+        }
+
         // footer load
         const footerPlaceholder = document.getElementById('footer-placeholder');
         if(footerPlaceholder) {
